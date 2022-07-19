b5472e89abc4c889dfd128b00a6bd5726aa8ee66
wincher <276545028@qq.com>
2022-07-19 10:00:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+
+const age = ref(0)
+console.log(age.value)
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
